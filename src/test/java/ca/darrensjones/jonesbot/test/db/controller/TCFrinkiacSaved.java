a6b67586b803b81637b786130091a62cb6c48cee
Darren Jones <darrenjones1991@gmail.com>
2020-12-09 11:22:23 -0600

    Added DB tests

---
+++ src/test/java/ca/darrensjones/jonesbot/test/db/controller/TCFrinkiacSaved.java
@@ -0,0 +1,73 @@
+package ca.darrensjones.jonesbot.test.db.controller;
+
+import java.util.List;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import ca.darrensjones.jonesbot.db.controller.CFrinkiacSaved;
+import ca.darrensjones.jonesbot.db.model.OFrinkiacSaved;
+
+/**
+ * @author Darren Jones
+ * @version 1.0.0 2020-12-09
+ * @since 1.0.0 2020-12-09
+ */
+public class TCFrinkiacSaved {
+
+	@Test
+	public void getById() {
+
+		// ID = 1: Simpsons
+		List<OFrinkiacSaved> s = CFrinkiacSaved.getById("1");
+		Assert.assertEquals(s.size(), 5);
+		Assert.assertEquals(s.get(0).id, 1);
+		Assert.assertEquals(s.get(0).name, "Trash");
+		Assert.assertEquals(s.get(0).key, "S03E22");
+		Assert.assertEquals(s.get(0).timestamp, "937738");
+		Assert.assertEquals(s.get(0).regex, "trash");
+		Assert.assertEquals(s.get(1).id, 2);
+		Assert.assertEquals(s.get(1).name, "Flag");
+		Assert.assertEquals(s.get(1).key, "S14E03");
+		Assert.assertEquals(s.get(1).timestamp, "883966");
+		Assert.assertEquals(s.get(1).regex, "flags?");
+		Assert.assertEquals(s.get(2).id, 3);
+		Assert.assertEquals(s.get(2).name, "Pig");
+		Assert.assertEquals(s.get(2).key, "Movie");
+		Assert.assertEquals(s.get(2).timestamp, "1321236");
+		Assert.assertEquals(s.get(2).regex, "(spider )?pig");
+		Assert.assertEquals(s.get(3).id, 4);
+		Assert.assertEquals(s.get(3).name, "Catholic Church");
+		Assert.assertEquals(s.get(3).key, "S10E12");
+		Assert.assertEquals(s.get(3).timestamp, "927876");
+		Assert.assertEquals(s.get(3).regex, "(catholic( church)?)|(we'?ve made a few changes)");
+		Assert.assertEquals(s.get(4).id, 5);
+		Assert.assertEquals(s.get(4).name, "The Anvil");
+		Assert.assertEquals(s.get(4).key, "S08E15");
+		Assert.assertEquals(s.get(4).timestamp, "856087");
+		Assert.assertEquals(s.get(4).regex, "(the anvil)|(gay steel mill)|(we work hard\\,? we play hard)");
+
+		// ID = 2: Futurama
+		List<OFrinkiacSaved> f = CFrinkiacSaved.getById("2");
+		Assert.assertEquals(f.size(), 2);
+		Assert.assertEquals(f.get(0).id, 6);
+		Assert.assertEquals(f.get(0).name, "Ass");
+		Assert.assertEquals(f.get(0).key, "S05E15");
+		Assert.assertEquals(f.get(0).timestamp, "586936");
+		Assert.assertEquals(f.get(0).regex, "ass");
+		Assert.assertEquals(f.get(1).id, 7);
+		Assert.assertEquals(f.get(1).name, "Lincoln");
+		Assert.assertEquals(f.get(1).key, "S03E10");
+		Assert.assertEquals(f.get(1).timestamp, "589838");
+		Assert.assertEquals(f.get(1).regex, "lincoln|jonesbot");
+
+		// ID = 3: Rick&Morty
+		List<OFrinkiacSaved> r = CFrinkiacSaved.getById("3");
+		Assert.assertEquals(r.size(), 1);
+		Assert.assertEquals(r.get(0).id, 8);
+		Assert.assertEquals(r.get(0).name, "TV");
+		Assert.assertEquals(r.get(0).key, "S01E08");
+		Assert.assertEquals(r.get(0).timestamp, "62479");
+		Assert.assertEquals(r.get(0).regex, "tv");
+	}
+}
--- src/test/resources/db/testdata.sql
+++ src/test/resources/db/testdata.sql
@@ -16,3 +16,13 @@ INSERT INTO reaction VALUES (':tophat:',	N'🎩',		'top hat');
 INSERT INTO reaction VALUES (':sandwich:',	N'🥪',		'sandwich(es)?');
 INSERT INTO reaction VALUES (':man_mage:',	N'🧙‍♂️',		'(mages?|wizards?)');
 INSERT INTO reaction VALUES (':flag_ca:',	N'🇨🇦',		'canada');
+
+--Frinkiac Saved
+INSERT INTO frinkiac_saved VALUES (1,	'Trash',			'S03E22',	'937738',	'trash');
+INSERT INTO frinkiac_saved VALUES (1,	'Flag',				'S14E03',	'883966',	'flags?');
+INSERT INTO frinkiac_saved VALUES (1,	'Pig',				'Movie',	'1321236',	'(spider )?pig');
+INSERT INTO frinkiac_saved VALUES (1,	'Catholic Church',	'S10E12',	'927876',	'(catholic( church)?)|(we''?ve made a few changes)');
+INSERT INTO frinkiac_saved VALUES (1,	'The Anvil',		'S08E15',	'856087',	'(the anvil)|(gay steel mill)|(we work hard\,? we play hard)');
+INSERT INTO frinkiac_saved VALUES (2,	'Ass',				'S05E15',	'586936',	'ass');
+INSERT INTO frinkiac_saved VALUES (2,	'Lincoln',			'S03E10',	'589838',	'lincoln|jonesbot');
+INSERT INTO frinkiac_saved VALUES (3,	'TV',				'S01E08',	'62479',	'tv');
