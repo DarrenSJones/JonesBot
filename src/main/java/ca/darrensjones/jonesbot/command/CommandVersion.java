fc1198910726522de76eb7e48716a77ff18adeba
Darren Jones <darrenjones1991@gmail.com>
2020-12-14 13:46:51 -0600

    Added version return command, only works for current version

--- src/main/java/ca/darrensjones/jonesbot/command/CommandVersion.java
+++ src/main/java/ca/darrensjones/jonesbot/command/CommandVersion.java
@@ -1,8 +1,13 @@
 package ca.darrensjones.jonesbot.command;
 
+import java.awt.Color;
+
 import ca.darrensjones.jonesbot.bot.Bot;
 import ca.darrensjones.jonesbot.command.meta.AbstractCommand;
 import ca.darrensjones.jonesbot.command.meta.CommandVisibility;
+import ca.darrensjones.jonesbot.db.controller.CVersion;
+import ca.darrensjones.jonesbot.db.model.OVersion;
+import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.entities.Message;
 
 /**
@@ -43,8 +48,13 @@ public class CommandVersion extends AbstractCommand {
 
 	@Override
 	public void execute(Message message) {
-		
-		
-		message.getChannel().sendMessage("Pong!").queue();
+		String current = bot.config.BOT_VERSION;
+		String[] v = current.split("\\.");
+		OVersion version = CVersion.getVersion(v[0], v[1], v[2]);
+		EmbedBuilder eb = new EmbedBuilder();
+		eb.setTitle("Current Version: " + current);
+		eb.setDescription(version.date.toString() + ": " + version.changes);
+		eb.setColor(new Color(0, 153, 255));
+		message.getChannel().sendMessage(eb.build()).queue();
 	}
 }
--- src/test/java/ca/darrensjones/jonesbot/test/db/controller/TCVersion.java
+++ src/test/java/ca/darrensjones/jonesbot/test/db/controller/TCVersion.java
@@ -20,11 +20,25 @@ public class TCVersion {
 	public void getAll() {
 		List<OVersion> l = CVersion.getAll();
 		Assert.assertEquals(l.size(), 1);
-
 		Assert.assertEquals(l.get(0).major, 1);
 		Assert.assertEquals(l.get(0).minor, 0);
 		Assert.assertEquals(l.get(0).patch, 0);
 		Assert.assertEquals(l.get(0).date, LocalDate.of(2020, 12, 14));
 		Assert.assertEquals(l.get(0).changes, "First version! Contains everything in the old Bot except the schedule.");
+	}
+
+	@Test(dependsOnMethods = "getAll", alwaysRun = true)
+	public void getVersion() {
+
+		// Valid
+		OVersion o = CVersion.getVersion("1", "0", "0");
+		Assert.assertEquals(o.major, 1);
+		Assert.assertEquals(o.minor, 0);
+		Assert.assertEquals(o.patch, 0);
+		Assert.assertEquals(o.date, LocalDate.of(2020, 12, 14));
+		Assert.assertEquals(o.changes, "First version! Contains everything in the old Bot except the schedule.");
+
+		// Invalid
+		Assert.assertNull(CVersion.getVersion("0", "0", "0"));
 	}
 }
