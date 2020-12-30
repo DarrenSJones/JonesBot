d3794ebdceb9c4a65e93bee8d171d28679881024
Darren Jones <darrenjones1991@gmail.com>
2020-12-29 20:40:34 -0600

    Created enum to manage the current game status

---
+++ src/main/java/ca/darrensjones/jonesbot/game/meta/GameStatus.java
@@ -0,0 +1,11 @@
+package ca.darrensjones.jonesbot.game.meta;
+
+/**
+ * @author Darren Jones
+ * @version 1.2.0 2020-12-29
+ * @since 1.2.0 2020-12-29
+ */
+public enum GameStatus {
+
+	RUNNING(), OVER();
+}
