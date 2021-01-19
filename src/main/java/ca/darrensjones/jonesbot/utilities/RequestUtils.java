ac538790b978b910ac92606222f03dfb4197d117
Darren Jones <darrenjones1991@gmail.com>
2021-01-19 10:05:55 -0600

    Updated logging to show that only the ResponseBody is returned.

--- src/main/java/ca/darrensjones/jonesbot/utilities/RequestUtils.java
+++ src/main/java/ca/darrensjones/jonesbot/utilities/RequestUtils.java
@@ -13,7 +13,7 @@ import ca.darrensjones.jonesbot.log.Reporter;
 
 /**
  * @author Darren Jones
- * @version 1.1.2 2021-01-13
+ * @version 1.1.3 2021-01-19
  * @since 1.0.0 2020-11-24
  */
 public class RequestUtils {
@@ -23,8 +23,8 @@ public class RequestUtils {
 		CloseableHttpClient httpClient = HttpClients.createDefault();
 		HttpGet httpGet = new HttpGet(new URI(requestUrl));
 		HttpEntity httpEntity = httpClient.execute(httpGet).getEntity();
-		String response = IOUtils.toString(httpEntity.getContent(), StandardCharsets.UTF_8.name());
-		Reporter.info(String.format("getResponseBody Received Response:[%s]", response));
-		return response;
+		String responseBody = IOUtils.toString(httpEntity.getContent(), StandardCharsets.UTF_8.name());
+		Reporter.info(String.format("getResponseBody Received ResponseBody:[%s]", responseBody));
+		return responseBody;
 	}
 }
