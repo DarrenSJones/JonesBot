2dac12dd73cd21fbe39c206a6477533c3a675ddc
Darren Jones <darrenjones1991@gmail.com>
2021-01-21 09:59:23 -0600

    Moved CVersion tests into their own file

---
+++ src/test/java/ca/darrensjones/jonesbot/test/db/controller/TCVersion.java
@@ -0,0 +1,29 @@
+package ca.darrensjones.jonesbot.test.db.controller;
+
+import java.util.List;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import ca.darrensjones.jonesbot.db.controller.CVersion;
+import ca.darrensjones.jonesbot.db.model.OVersion;
+
+/**
+ * @author Darren Jones
+ * @version 1.1.4 2021-01-21
+ * @since 1.1.4 2021-01-21
+ */
+public class TCVersion {
+
+	@Test
+	public void getVersionList() {
+		List<OVersion> versions = CVersion.getVersionList();
+
+		for (OVersion version : versions) {
+			Assert.assertTrue(Integer.toString(version.major).matches("\\d"));
+			Assert.assertTrue(Integer.toString(version.minor).matches("\\d"));
+			Assert.assertTrue(Integer.toString(version.patch).matches("\\d"));
+			Assert.assertTrue(version.getName().matches("\\d\\.\\d\\.\\d"));
+		}
+	}
+}
