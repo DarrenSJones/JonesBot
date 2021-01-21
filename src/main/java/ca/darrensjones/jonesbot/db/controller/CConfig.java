c978deb520ccf14c5f81ca6115ffe9cb953f7be3
Darren Jones <darrenjones1991@gmail.com>
2021-01-21 08:34:43 -0600

    Added method to get only DB_Version from the bot_config table

--- src/main/java/ca/darrensjones/jonesbot/db/controller/CConfig.java
+++ src/main/java/ca/darrensjones/jonesbot/db/controller/CConfig.java
@@ -8,7 +8,7 @@ import ca.darrensjones.jonesbot.log.Reporter;
 
 /**
  * @author Darren Jones
- * @version 1.1.3 2021-01-18
+ * @version 1.1.4 2021-01-20
  * @since 1.0.0 2020-11-18
  */
 public class CConfig {
@@ -23,5 +23,17 @@ public class CConfig {
 			Reporter.fatal(e.getMessage());
 		}
 		return new String[] { map.get("BOT_PREFIX"), map.get("BOT_TOKEN"), map.get("BOT_OWNER_ID"), map.get("BOT_GITHUB_REPO"), map.get("WEATHER_TOKEN") };
+	}
+
+	public static String getDatabaseVersion() {
+		String version = "0.0.0";
+		try {
+			ResultSet rs = BotDB.get().select("SELECT item_key, item_value FROM bot_config WHERE item_key = 'DB_VERSION'");
+			version = rs.getString("item_value");
+			rs.getStatement().close();
+		} catch (Exception e) {
+			Reporter.fatal(e.getMessage());
+		}
+		return version;
 	}
 }
