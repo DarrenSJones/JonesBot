c19986af87fd079c1a69bb425265f17f81be62ef
Darren Jones <darrenjones1991@gmail.com>
2021-01-29 21:57:05 -0600

    Added first version of DBUpdater, run in Eclipse only currently.

---
+++ src/main/java/ca/darrensjones/jonesbot/db/DBUpdater.java
@@ -0,0 +1,93 @@
+package ca.darrensjones.jonesbot.db;
+
+import java.io.File;
+import java.nio.charset.StandardCharsets;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.commons.io.FileUtils;
+
+import ca.darrensjones.jonesbot.log.Reporter;
+
+/**
+ * @author Darren Jones
+ * @version 1.1.4 2021-01-29
+ * @since 1.1.4 2021-01-29
+ */
+public class DBUpdater {
+
+	// Executed from the command line
+	public static void main(String[] args) {
+		String test = "jonesbottest";
+		BotDB.set("localhost:1433", test, "jonesbot", "jonesbot");
+		dropAllTables(test);
+		updateToVersion(test);
+		populateTable(test, "src/test/resources/db/testdata.sql");
+
+		String prod = "jonesbot";
+		BotDB.set("localhost:1433", prod, "jonesbot", "jonesbot");
+		dropAllTables(prod);
+		updateToVersion(prod);
+		populateTable(prod, "C:\\git\\data\\jonesbot_data.sql");
+	}
+
+	private static void dropAllTables(String databaseName) {
+		try {
+			Reporter.info(String.format("Dropping all tables from [%s].", databaseName));
+
+			List<String> tables = new ArrayList<String>();
+			ResultSet rs = BotDB.get().select(String.format("USE %s SELECT name FROM sys.tables;", databaseName));
+			while (rs.next()) tables.add(rs.getString("name"));
+			rs.getStatement().close();
+			Reporter.info(String.format("Found [%s] tables in the database [%s].", tables.size(), databaseName));
+
+			for (String table : tables) {
+				String query = String.format("DROP TABLE [%s];", table);
+				BotDB.get().execute(query);
+				Reporter.info(String.format("JDBC Execute:[%s]", query));
+			}
+
+			Reporter.info(String.format("Dropped all tables from [%s].", databaseName));
+
+		} catch (Exception e) {
+			Reporter.fatal(e.getMessage());
+		}
+	}
+
+	private static void updateToVersion(String databaseName) {
+		try {
+			Reporter.info(String.format("Updating database [%s] to version [1.0.0]", databaseName));
+
+			String[] queries = FileUtils.readFileToString(new File("src/main/resources/db/1_0_0.sql"), StandardCharsets.UTF_8).split("(?<=;)");
+
+			for (String query : queries) {
+				query = query.replaceAll("--[\\s\\S]+\\n", "").replaceAll("\\s+", " ").trim();
+				BotDB.get().execute(query);
+				Reporter.info(String.format("JDBC Execute:[%s]", query));
+			}
+
+			Reporter.info(String.format("Updated database [%s] to version [1.0.0]", databaseName));
+		} catch (Exception e) {
+			Reporter.fatal(e.getMessage());
+		}
+	}
+
+	private static void populateTable(String databaseName, String dataPath) {
+		try {
+			Reporter.info(String.format("Populating Database:[%s].", databaseName));
+
+			String[] queries = FileUtils.readFileToString(new File(dataPath), StandardCharsets.UTF_8).split("(?<=;)");
+
+			for (String query : queries) {
+				query = query.replaceAll("--[\\s\\S]+\\n", "").replaceAll("\\s+", " ").trim();
+				int updates = BotDB.get().update(query);
+				Reporter.info(String.format("JDBC Execute, inserted [%s] Row(s):[%s]", updates, query));
+			}
+
+			Reporter.info(String.format("Populated Database:[%s].", databaseName));
+		} catch (Exception e) {
+			Reporter.fatal(e.getMessage());
+		}
+	}
+}
