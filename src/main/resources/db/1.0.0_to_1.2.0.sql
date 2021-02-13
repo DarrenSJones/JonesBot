1fd58ff08b7402f5905a14268dcef60fd823930c
Darren Jones <darrenjones1991@gmail.com>
2021-02-12 20:44:44 -0600

    Fixed column names

--- src/main/resources/db/1.0.0_to_1.2.0.sql
+++ src/main/resources/db/1.0.0_to_1.2.0.sql
@@ -1,8 +1,8 @@
 CREATE TABLE "simple_schedule" (
-	"id"			INT IDENTITY(1, 1)	NOT NULL	PRIMARY KEY,
-	"date"			DATE,
-	"day_of_week"	VARCHAR(9),
-	"guild_id"		CHAR(18)			NOT NULL,
-	"channel_id"	CHAR(18)			NOT NULL,
-	"time"			VARCHAR(32)			NOT NULL,
-	"value"			VARCHAR(128)		NOT NULL);
+	"id"				INT IDENTITY(1, 1)	NOT NULL	PRIMARY KEY,
+	"event_date"		DATE,
+	"event_day_of_week"	VARCHAR(9),
+	"guild_id"			CHAR(18)			NOT NULL,
+	"channel_id"		CHAR(18)			NOT NULL,
+	"event_time"		VARCHAR(32)			NOT NULL,
+	"event_value"		VARCHAR(128)		NOT NULL);
