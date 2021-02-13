98f50ee8d8b8d8f44c4370dfd3e333003073c803
Darren Jones <darrenjones1991@gmail.com>
2021-02-13 15:13:21 -0600

    Added cron scheduler to trigger the simple schedule at preset times

--- src/main/java/ca/darrensjones/jonesbot/main/Main.java
+++ src/main/java/ca/darrensjones/jonesbot/main/Main.java
@@ -3,10 +3,12 @@ package ca.darrensjones.jonesbot.main;
 import ca.darrensjones.jonesbot.bot.Bot;
 import ca.darrensjones.jonesbot.db.BotDB;
 import ca.darrensjones.jonesbot.log.Reporter;
+import ca.darrensjones.jonesbot.simpleschedule.SimpleSchedule;
+import it.sauronsoftware.cron4j.Scheduler;
 
 /**
  * @author Darren Jones
- * @version 1.1.4 2021-02-01
+ * @version 1.2.0 2021-02-13
  * @since 1.0.0 2020-11-18
  */
 public class Main {
@@ -18,7 +20,12 @@ public class Main {
 		BotDB.init();
 		Bot bot = new Bot();
 		bot.setJDA(); // Connects to Discord using JDA
+		Reporter.info(String.format("Bot Initialized! Logged into Discord as:[%s]", bot.jda.getSelfUser().toString()));
 
-		Reporter.info(String.format("Bot Initialized! Logged in as:[%s]", bot.jda.getSelfUser().toString()));
+		String cron = "0 6 * * *";
+		Scheduler scheduler = new Scheduler();
+		scheduler.schedule(cron, new Thread(() -> SimpleSchedule.scheduleCron(bot)));
+		scheduler.start();
+		Reporter.info(String.format("Scheduler Initialized! Cron:[%s]", cron));
 	}
 }
