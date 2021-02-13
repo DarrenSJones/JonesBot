6222f116bc1cc75afe3cb3e563a5cc34d499e606
Darren Jones <darrenjones1991@gmail.com>
2021-02-12 18:05:32 -0600

    Changed simple_schedule day_of_week column to String instead of int

--- src/main/resources/db/1.0.0_to_1.2.0.sql
+++ src/main/resources/db/1.0.0_to_1.2.0.sql
@@ -1,7 +1,7 @@
 CREATE TABLE "simple_schedule" (
 	"id"			INT IDENTITY(1, 1)	NOT NULL	PRIMARY KEY,
 	"date"			DATE,
-	"day_of_week"	INT,
+	"day_of_week"	VARCHAR(9),
 	"guild_id"		CHAR(18)			NOT NULL,
 	"channel_id"	CHAR(18)			NOT NULL,
 	"time"			VARCHAR(32)			NOT NULL,
